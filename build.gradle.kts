c01033be5f74b2d1f8765661d3cf5696f8fcc3b2
David Steinsland <david@steinsland.email>
2022-04-05 08:41:32 +0200

    bumper rapids-and-rivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val testcontainersVersion = "1.16.2"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.github.navikt:rapids-and-rivers:2022.04.04-22.16.0611abb2a604")
+    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-08.01.237f724ae572")
 
     implementation("org.postgresql:postgresql:42.3.1")
     implementation("com.zaxxer:HikariCP:$hikariVersion")
