a9e331790d4e57a0efde87d5d4a0f0e08d949036
david steinsland <david@davidsteinsland.net>
2024-11-12 20:58:02 +0100

    bumper versjoner

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,9 @@ val flywayVersion = "10.8.1"
 val hikariVersion = "5.1.0"
 val kotliqueryVersion = "1.9.0"
 val junitJupiterVersion = "5.11.3"
-val tbdLibsVersion = "2024.11.12-11.09-16cf2599"
+val tbdLibsVersion = "2024.11.12-20.46-e1fd6a25"
 val mockkVersion = "1.13.12"
-val rapidsAndRiversVersion = "2024111211071731406062.648687519469"
+val rapidsAndRiversVersion = "2024111220531731441232.6f0a7a6c643b"
 
 dependencies {
     implementation("com.github.navikt:rapids-and-rivers:$rapidsAndRiversVersion")
