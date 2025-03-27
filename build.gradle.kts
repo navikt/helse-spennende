fa07d947b14c8152c9103bc699423eb9b8873a9a
david steinsland <david@davidsteinsland.net>
2025-03-27 20:15:57 +0100

    bumper avhengigheter

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,14 +19,14 @@ repositories {
     maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
 }
 
-val flywayVersion = "10.21.0"
-val hikariCPVersion = "6.1.0"
+val flywayVersion = "11.5.0"
+val hikariCPVersion = "6.3.0"
 val kotliqueryVersion = "1.9.0"
-val junitJupiterVersion = "5.11.3"
-val tbdLibsVersion = "2025.01.27-12.50-76316f3b"
-val mockkVersion = "1.13.12"
-val rapidsAndRiversVersion = "2025012712551737978926.de930d8e0feb"
-val postgresqlVersion = "42.7.4"
+val junitJupiterVersion = "5.12.1"
+val tbdLibsVersion = "2025.03.27-18.30-c228796d"
+val mockkVersion = "1.13.17"
+val rapidsAndRiversVersion = "2025032719521743101565.4abbf0c3af65"
+val postgresqlVersion = "42.7.5"
 
 dependencies {
     implementation("com.github.navikt:rapids-and-rivers:$rapidsAndRiversVersion")
