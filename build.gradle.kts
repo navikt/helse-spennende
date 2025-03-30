e598cef63066f5ed0af124f722585a25310f7bbb
david steinsland <david@davidsteinsland.net>
2025-03-30 13:23:17 +0200

    bumper r&r

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,9 +23,9 @@ val flywayVersion = "11.5.0"
 val hikariCPVersion = "6.3.0"
 val kotliqueryVersion = "1.9.0"
 val junitJupiterVersion = "5.12.1"
-val tbdLibsVersion = "2025.03.27-18.30-c228796d"
+val tbdLibsVersion = "2025.03.30-13.02-f7cb11ef"
 val mockkVersion = "1.13.17"
-val rapidsAndRiversVersion = "2025032719521743101565.4abbf0c3af65"
+val rapidsAndRiversVersion = "2025033013081743332933.20de2f3d8983"
 val postgresqlVersion = "42.7.5"
 
 dependencies {
